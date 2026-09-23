4db80d71d0bbf835f85b9b0120583b7a92e7d5f5
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:52:36 -0400

    xuyisen__diff__id_186

--- libs/agno/agno/workflow/v2/types.py
+++ libs/agno/agno/workflow/v2/types.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from pydantic import BaseModel
 
